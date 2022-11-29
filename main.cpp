f1250ceb46ad9fc351028bcceb1cdea200930f8d
Lxixnxuxs <linusmeierhfer@yahoo.com>
2022-11-29 14:38:03 +0100

    changed main

--- main.cpp
+++ main.cpp
@@ -5,26 +5,13 @@
 #include "ParserMain.hpp"
 
 int main() {
-/*
-    string filename = "/home/linus/transfer/wsl_transfer/H2/resources/program.txt";
-    Parser P;
-    std::ifstream fs(filename);
-
-    P.read(fs);
-
-    for (ParsedFunction f : P.split_by_functions()) {
-        //std::cout << f.name << "|" << f.arguments << "|" << f.type << "|" << f.expression_block << "\n";
-
-        for (auto e : f.extract_statements()) {
-            std::cout << e << "\n";
-        }
-    }
-    */
-
-    string filename = "/home/linus/transfer/wsl_transfer/H2/resources/program.txt";
+    std::string filename = "/home/linus/transfer/wsl_transfer/H2/resources/program.txt";
     ParserMain p(filename);
 
-    std::cout << p.compile() << endl;
+    //std::cout << p.compile() << endl;
+    std::ofstream out("as.s");
+    out << p.compile();
+    out.close();
 
     return 0;
 }
