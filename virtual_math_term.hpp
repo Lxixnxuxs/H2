185f734137b91ebd62e778316bf7bc219d1cf3b8
Ray Klauck <rayklauck@web.de>
2023-01-16 08:34:24 +0100

    greatly improved simplifying logic

--- virtual_math_term.hpp
+++ virtual_math_term.hpp
@@ -23,104 +23,34 @@ struct VirtualMathTerm {
 
     VirtualMathTerm(MathTermType type) : type(type) {}
 
-    VirtualMathTerm(std::string name) : type(VARIABLE), name(name) {}
+    VirtualMathTerm(std::string name) {
+        try {
+            double nr = stoi(name);
+            *this = VirtualMathTerm(nr); // is this a correct way of calling another constructor?
+        } catch (...) {
+            type = VARIABLE;
+            this->name = name;
+        }
+    }
 
     VirtualMathTerm(double value) : type(NUMBER), value(value) {}
 
     VirtualMathTerm()=default;
 
-    virtual void simplify() {
-        if (type == NUMBER or type == VARIABLE) return;
 
-        // first simplify lower levels
-        for (auto& e: children) e.simplify();
+    // method for calling simplify logic until a steady state is reached
+    void simplify() {
+        auto before = *this;
+        simplify_logic();
+        if (o_notation) turn_into_o_notation();
 
-        if (type == EXPONENTIAL) {
-            assert(children.size() == 2);
-            if (children[0].type == NUMBER and children[1].type == NUMBER) {
-                // transform to be only one number
-                value = pow(children[0].value, children[1].value);
-                type = NUMBER;
-                children = {};
-            }
-            return;
-        }
-
-        // putting together multiple Number values
-        double default_value;
-        double higher_default_value;
-        std::function<double(double,double)> combination_func;
-        MathTermType higher_type;
-
-        if (type == ADDITION) {
-            default_value = 0;
-            combination_func = [](double a, double b){return a+b;};
-            higher_type = MULTIPLICATION;
-            higher_default_value = 1;
-
-        } else if (type == MULTIPLICATION) {
-            default_value = 1;
-            combination_func = [](double a, double b){return a*b;};
-            higher_type = EXPONENTIAL;
-            higher_default_value = 1;
-        }
-
-
-
-            double over_all_value = default_value;
-            std::vector<VirtualMathTerm> new_children;
-            std::map<std::string,double> var_to_occurances;
-
-            for (auto e: children) {
-                if (e.type == NUMBER) {
-                    over_all_value = combination_func(over_all_value,e.value);
-                } else if (e.type == VARIABLE) {
-                    bool found = false;
-
-                    // does this need to be initialized first?
-                    for (auto p : var_to_occurances) {
-                        if (p.first == e.name) found = true;
-                    }
-                    if (!found) var_to_occurances[e.name]=0;
-                    var_to_occurances[e.name] += 1;
-                } else {
-                    new_children.push_back(e);
-                }
-            }
-
-            // unify all Variable - Occurances into one Exponential
-            for (auto p : var_to_occurances) {
-                if (p.second == higher_default_value) {
-                    new_children.emplace_back(p.first);
-                } else {
-                    auto higher = VirtualMathTerm(higher_type);
-                    higher.children.emplace_back(p.first);
-                    higher.children.emplace_back(p.second);
-                    new_children.push_back(higher);
-                }
-            }
-            if (over_all_value != default_value) new_children.emplace_back(over_all_value);
-            children = new_children;
-
-            /*
-            if (children.empty()) {
-                children.emplace_back(default_value);
-            }*/
-
-            // simplify to be just the child
-            if (children.size() == 1) {
-                copy_to_me(children[0]);
-                return;
-            }
-
-
-
-            //if (o_notation) turn_into_o_notation();
+        // keep on simplifying, since term was still changing
+        if (before!=*this) simplify();
     }
 
     std::string as_string() {
         simplify();
-        if (o_notation) turn_into_o_notation();
+
         std::string res;
 
         if (type == NUMBER){
@@ -133,7 +63,7 @@ struct VirtualMathTerm {
         if (type == ADDITION or type == MULTIPLICATION) {
             std::string op_symbol = (type == ADDITION) ? "+" : "*";
 
-            //res += std::string(fmax(children.size()-2,0),'(');
+            res += std::string(fmax(children.size()-2,0),'(');
 
             for (int i = 0; i < children.size(); i++) {
                 if (i != 0) res += " " + op_symbol + " ";
@@ -143,7 +73,7 @@ struct VirtualMathTerm {
                 if (!is_primitive) res += "(";
                 res += children[i].as_string();
                 if (!is_primitive) res += ")";
-                //if (i>=2) res += ")";
+                if (i>=1 and i<children.size()-1) res += ")";
 
             }
 
@@ -152,9 +82,164 @@ struct VirtualMathTerm {
         return res;
     }
 
+    bool operator==(const VirtualMathTerm& other) const {
+        if (type != other.type) return false;
+
+        if (type == NUMBER) return (value == other.value);
+        if (type == VARIABLE) return (name == other.name);
+
+        // is Addition, Multiplication or Exponential
+        if (children.size() != other.children.size()) return false;
+
+        for (int i = 0; i<children.size(); i++) {
+            if (!(children[i]==other.children[i])) return false;
+        }
+        return true;
+    }
+
+    bool operator!=(const VirtualMathTerm& other) {
+        return !operator==(other);
+    }
+
+private:
+    void copy_to_me(VirtualMathTerm other) {
+        type = other.type;
+        name = other.name;
+        value = other.value;
+        children = other.children;
+    }
+
+    void simplify_same_nested_operation(){
+        assert(type == ADDITION or type == MULTIPLICATION);
+
+        // taking lower levels of the same operation to this level
+        std::vector<VirtualMathTerm> new_children;
+        for (auto e : children) {
+            if (type != e.type) new_children.push_back(e);
+            else {
+                for (auto c : e.children) new_children.push_back(c);
+            }
+        }
+        children = new_children;
+    }
+
+    // putting together multiple Number values
+    void simplify_multiple_numbers(){
+        assert(type == ADDITION or type == MULTIPLICATION);
+        double default_value;
+        std::function<double(double,double)> combination_func;
+
+        if (type == ADDITION) {
+            default_value = 0;
+            combination_func = [](double a, double b){return a+b;};
+
+        } else if (type == MULTIPLICATION) {
+            default_value = 1;
+            combination_func = [](double a, double b){return a*b;};
+        }
+
+
+        double over_all_value = default_value;
+        std::vector<VirtualMathTerm> new_children;
+        std::map<std::string,double> var_to_occurances;
+
+        for (auto e: children) {
+            if (e.type == NUMBER) {
+                over_all_value = combination_func(over_all_value,e.value);
+            } else new_children.push_back(e);
+        }
+
+        if (over_all_value != default_value) new_children.emplace_back(over_all_value);
+        children = new_children;
+    }
+
+    void simplify_variable_occurances() {
+        assert(type == ADDITION or type == MULTIPLICATION);
+        double higher_default_value;
+        MathTermType higher_type;
+
+        if (type == ADDITION) {
+            higher_type = MULTIPLICATION;
+            higher_default_value = 1;
+
+        } else if (type == MULTIPLICATION) {
+            higher_type = EXPONENTIAL;
+            higher_default_value = 1;
+        }
+
+
+        std::vector<VirtualMathTerm> new_children;
+        std::map<std::string,double> var_to_occurances;
+
+        for (auto e: children) {
+            if (e.type == VARIABLE) {
+                bool found = false;
+
+                // does this need to be initialized first?
+                for (auto p : var_to_occurances) {
+                    if (p.first == e.name) found = true;
+                }
+                if (!found) var_to_occurances[e.name]=0;
+                var_to_occurances[e.name] += 1;
+            } else {
+                new_children.push_back(e);
+            }
+        }
+
+
+        for (auto p : var_to_occurances) {
+            if (p.second == higher_default_value) {
+                new_children.emplace_back(p.first);
+            } else {
+                auto higher = VirtualMathTerm(higher_type);
+                higher.children.emplace_back(p.first);
+                higher.children.emplace_back(p.second);
+                new_children.push_back(higher);
+            }
+        }
+        children = new_children;
+    }
+
+    void simplify_one_child() {
+        if (children.size() == 1) {
+            copy_to_me(children[0]);
+            return;
+        }
+    }
+
+    void simplify_logic() {
+        if (type == NUMBER or type == VARIABLE) return;
+
+        // first simplify lower levels
+        for (auto& e: children) e.simplify_logic();
+
+        if (type == EXPONENTIAL) {
+            assert(children.size() == 2);
+            if (children[0].type == NUMBER and children[1].type == NUMBER) {
+                // transform to be only one number
+                value = pow(children[0].value, children[1].value);
+                type = NUMBER;
+                children = {};
+            }
+            return;
+        }
+
+        simplify_same_nested_operation();
+        simplify_multiple_numbers();
+        simplify_variable_occurances();
+        simplify_one_child();
+    }
+
+
     // this is a kind of simplification function which will cut off constants and will provide pure O-Notation
     void turn_into_o_notation() {
-        if (type == NUMBER or type == VARIABLE) { return;}
+        if (type == VARIABLE) { return;}
+        if (type == NUMBER) {
+            value = 1;
+            return;
+        }
+
+        //auto before = *this; // create copy to compare in the end
 
         for (auto& e : children) e.turn_into_o_notation();
 
@@ -167,14 +252,13 @@ struct VirtualMathTerm {
         }
         if (new_children.empty()) new_children.emplace_back(1);
         children = new_children;
-    }
 
-private:
-    void copy_to_me(VirtualMathTerm other) {
-        type = other.type;
-        name = other.name;
-        value = other.value;
-        children = other.children;
+        if (new_children.size() == 1){
+            copy_to_me(children[0]);
+        }
+
+        // keep on simplifying, since term was still changing
+        //if (before != *this) simplify();
     }
 };
 
